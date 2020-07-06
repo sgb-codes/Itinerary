083b2d5398ea001e427ff29e6016e798d6ac37f8
Simon Barrett <sbarrett765@gmail.com>
2020-07-06 17:02:34 +0100

    Added DispatchQueue to readTrips

--- Itinerary/Model/Trip/TripFunctions.swift
+++ Itinerary/Model/Trip/TripFunctions.swift
@@ -13,11 +13,16 @@ class TripFunctions {
         
     }
     
-    static func readTrip() {
-        if Data.tripModels.count == 0 {
-            Data.tripModels.append(TripModel(title: "Trip to Bali!"))
-            Data.tripModels.append(TripModel(title: "Mexico"))
-            Data.tripModels.append(TripModel(title: "Russian Trip"))
+    static func readTrip(completion: @escaping () -> ()) {
+        DispatchQueue.global(qos: .userInteractive).async {
+            if Data.tripModels.count == 0 {
+                Data.tripModels.append(TripModel(title: "Trip to Bali!"))
+                Data.tripModels.append(TripModel(title: "Mexico"))
+                Data.tripModels.append(TripModel(title: "Russian Trip"))
+            }
+        }
+        DispatchQueue.main.async {
+            completion()
         }
     }
     
