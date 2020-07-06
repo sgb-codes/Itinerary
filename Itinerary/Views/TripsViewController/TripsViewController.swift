8488a822f778167fe82f44d8ab30aca8438dad77
Simon Barrett <sbarrett765@gmail.com>
2020-07-06 17:45:36 +0100

    Data Populating TableView for Trips

---
+++ Itinerary/Views/TripsViewController/TripsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TripsViewController.swift
+//  Itinerary
+//
+//  Created by Simon Barrett on 06/07/2020.
+//  Copyright © 2020 Simon Barrett. All rights reserved.
+//
+
+import UIKit
+
+class TripsViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        TripFunctions.readTrip { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.tripModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        if cell == nil {
+         cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        }
+        
+        cell!.textLabel?.text = Data.tripModels[indexPath.row].title
+        
+        return cell!
+    }
+}
